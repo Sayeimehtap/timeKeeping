882c9451e1242ab3c77950026bb1bc5a3f900cff
sayeimehtap <sayeimehtap@gmail.com>
2020-06-17 23:37:47 +0300

    animasyon eklendi

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -16,16 +16,15 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
     HomePageRoutingModule,
 
     NgCircleProgressModule.forRoot({
-      // set defaults here
-      radius: 100,
-      outerStrokeWidth: 16,
-      innerStrokeWidth: 8,
-      outerStrokeColor: "#78C000",
-      innerStrokeColor: "#C7E596",
-      animationDuration: 300,
-      animation: false,
-      responsive: true,
-      renderOnClick: false,
+      "space": 0,
+      "imageSrc": "assets/notification.svg",
+      "imageHeight": 154,
+      "imageWidth": 154,
+      "showImage": false,
+      "showUnits": true,
+      "showBackground": true,
+      "responsive": true,
+      "animation": false
     })
   ],
   declarations: [HomePage]
